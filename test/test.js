f0b40794080ffb748bae636fc3eee55de53c41a3
Austin Kelleher <austin.kell47@gmail.com>
2017-11-06 19:09:16 -0500

    Move to using ava for tests.

--- test/test.js
+++ test/test.js
@@ -1,73 +1,57 @@
 /*jshint -W030 */
 'use strict';
 
+const test = require('ava')
 const conflogger = require('../');
-const chai = require('chai');
-const expect = chai.expect;
 
-chai.should();
-chai.config.includeStack = true;
+test('should return configured logger if true passed', (t) => {
+  const logger = conflogger.configure(true);
+  t.is(typeof logger.info, 'function')
+  logger.info('Should print this.');
+});
 
-describe('Conflogger tests', function() {
-  it('should return configured logger if true passed', () => {
-    const logger = conflogger.configure(true);
-    expect(logger.info).to.be.a('function');
+test('should return noop logger if false passed', (t) => {
+  const logger = conflogger.configure(false);
+  t.is(typeof logger.info, 'function')
+  logger.error('Should not print this.');
+});
 
-    logger.info('Should print this.');
-  });
-
-  it('should return noop logger if false passed', () => {
-    const logger = conflogger.configure(false);
-    expect(logger.info).to.be.a('function');
-
-    logger.error('Should not print this.');
-  });
-
-  it('should add noop methods to logger that is passed without all logging functions', () => {
-    let original = {
-      info(msg) {
-        console.log(msg);
-      }
-    };
-
-    const logger = conflogger.configure(original);
-
-    expect(logger.info).to.be.a('function');
-    expect(logger.fatal).to.be.a('function');
-
-    logger.info('Should print this');
-    logger.fatal('Should print this');
-
-    expect(original.info).to.be.a('function');
-    expect(original.fatal).to.be.a('function');
-
-    original.info('Should print this');
-    original.fatal('Should print this');
-  });
-
-  it('should return noop logger if no logger passed', () => {
-    const logger = conflogger.configure();
-    expect(logger.info).to.be.a('function');
-
-    logger.info('Should not print this');
-    logger.fatal('Should not print this');
-  });
-
-  it('should accept a log level and only print within the level', () => {
-    const logger = conflogger.configure(true, {
-      logLevel: conflogger.logLevels.DEBUG
-    });
-
-    if (!logger.isDebugEnabled()) {
-      throw new Error('DEBUG mode should be enabled.');
+test('should add noop methods to logger that is passed without all logging functions', (t) => {
+  let original = {
+    info(msg) {
+      console.log(msg);
     }
+  };
 
-    if (!logger.isInfoEnabled()) {
-      throw new Error('INFO mode should be enabled.');
-    }
+  const logger = conflogger.configure(original);
 
-    if (logger.isTraceEnabled()) {
-      throw new Error('Trace should not be enabled.');
-    }
+  t.is(typeof logger.info, 'function')
+  t.is(typeof logger.fatal, 'function')
+
+  logger.info('Should print this');
+  logger.fatal('Should print this');
+
+  t.is(typeof original.info, 'function')
+  t.is(typeof original.fatal, 'function')
+
+  original.info('Should print this');
+  original.fatal('Should print this');
+});
+
+test('should return noop logger if no logger passed', (t) => {
+  const logger = conflogger.configure();
+  t.is(typeof logger.info, 'function')
+
+  logger.info('Should not print this');
+  logger.fatal('Should not print this');
+});
+
+test('should accept a log level and only print within the level', (t) => {
+  const logger = conflogger.configure(true, {
+    logLevel: conflogger.logLevels.DEBUG
   });
+
+  t.true(logger.isDebugEnabled())
+  t.true(logger.isInfoEnabled())
+  t.false(logger.isTraceEnabled())
 });
